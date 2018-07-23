c85ba4ac020e1c32a58eb4a4f904546bbdd70470
Vigen Oganesyan <38381948+ArmenianButcher@users.noreply.github.com>
2018-07-23 13:13:45 +0300

    Create begin-7.cpp

---
+++ begin-7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	const float pi = 3.14;
+	int R;
+	cin >> R;
+	cout << "Length: " << 2*pi*R << endl;
+	cout << "Area: " << pi*R*R << endl;
+	return 0;
+} 
