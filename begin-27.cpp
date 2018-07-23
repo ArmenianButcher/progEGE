daa0a3bfa2fd20d49cb83be35c86941a7e1b4027
Vigen Oganesyan <38381948+ArmenianButcher@users.noreply.github.com>
2018-07-23 13:21:27 +0300

    Create begin-27.cpp

---
+++ begin-27.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int A;
+	cin >> A;
+	int HELP = A;
+	A = A*HELP;
+	cout << A << endl;
+	A = A*A;
+	cout << A << endl;
+	A = A*A;
+	cout << A << endl;
+	return 0;
+} 
