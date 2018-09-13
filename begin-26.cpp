8d9dc4cd93d5f52c2f389248b7659941abd4f07e
Vigen Oganesyan <38381948+ArmenianButcher@users.noreply.github.com>
2018-09-13 21:29:02 +0300

    Create begin-26.cpp

---
+++ begin-26.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<math.h>
+using namespace std;
+
+
+
+int main() {
+	double x;
+	cin >> x;
+	cout << 4.0*pow(x-3.0,3.0)-7.0*pow(x-3.0,3.0) +2.0 << endl;
+    return 0;
+}
