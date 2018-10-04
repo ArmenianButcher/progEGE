3612a7ade3c3bf156ed754450f1b61fbadaed21e
Vigen Oganesyan <38381948+ArmenianButcher@users.noreply.github.com>
2018-10-04 21:59:40 +0300

    Update for-14.cpp

--- for-14.cpp
+++ for-14.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int squareOfNum(int A) {
 	int result = 1;
+	cout << 1 << endl;
 	for(int i = 1;i<A;i++) {
+		cout << (2*i+1) << endl;
 		result+=(2*i+1);
 	}
 	return result;
